3a54c7f9816cae6bd124117fbd5ed7aa6c397644
Johandry Mora <johandrydev@gmail.com>
2024-10-06 16:42:10 -0500

    Update migrations

--- migrations/003_payment_methods.sql
+++ migrations/003_payment_methods.sql
@@ -14,14 +14,10 @@ CREATE TABLE payment_methods
 ALTER TABLE payment_methods
     ADD CONSTRAINT fk_payment_methods_owner_id FOREIGN KEY (owner_id) REFERENCES customers (id);
 
-BEGIN;
-
 INSERT INTO payment_methods (owner_id, type, product_number, expiration_date, created_at)
 VALUES
     ((SELECT id FROM customers WHERE email = 'pablo@client.com'), 'CREDIT_CARD', '4242424242424242', '2025-12-31', now()),
-    ((SELECT id FROM customers WHERE email = 'maria@example.com'),'DEBIT_CARD', '4242424242424242', '2024-11-30', now()),
-
-COMMIT;
+    ((SELECT id FROM customers WHERE email = 'maria@example.com'),'DEBIT_CARD', '4242424242424242', '2024-11-30', now());
 
 ---- create above / drop below ----
 

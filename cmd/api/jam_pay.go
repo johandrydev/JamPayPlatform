62d5bcf07de6eadf61083aeb2182670b8691a771
Johandry Mora <johandrydev@gmail.com>
2024-10-06 23:49:47 -0500

    Add login routes, add middlewares to routes and update migrations

--- cmd/api/jam_pay.go
+++ cmd/api/jam_pay.go
@@ -10,6 +10,7 @@ import (
 	merchant "JamPay/internal/handler"
 	"JamPay/internal/payment_services/stripe"
 	"JamPay/internal/pkg/database"
+	"JamPay/internal/pkg/middleware"
 )
 
 const port = "8080"
@@ -29,18 +30,32 @@ func main() {
 	stripeProvider := stripe.NewStripeService()
 	paymentHandlers := merchant.NewPaymentHandler(db, stripeProvider)
 
+	authHandler := merchant.NewAuthHandler(db)
+
 	router := chi.NewRouter()
 	router.Route("/api", func(r chi.Router) {
-		r.Route("/merchant", func(r chi.Router) {
-			r.Get("/{merchantID}", merchanHandlers.FindMerchant)
-			r.Get("/{merchantID}/payments", paymentHandlers.GetAllByMerchantID)
-		})
+		r.Post("/login", authHandler.Login)
 
-		r.Route("/payment", func(r chi.Router) {
-			r.Post("/", paymentHandlers.CreatePayment)
-			r.Get("/{paymentID}", paymentHandlers.GetPayment)
-			r.Post("/{paymentID}/process", paymentHandlers.ProcessPayment)
-			r.Post("/{paymentID}/refund", paymentHandlers.RefundPayment)
+		r.Group(func(r chi.Router) {
+			r.Use(middleware.IsAuthenticated)
+
+			r.Route("/merchant", func(r chi.Router) {
+				r.Get("/{merchantID}", merchanHandlers.FindMerchant)
+
+				r.With(middleware.IsMerchant).Route("/{merchantID}/payments", func(r chi.Router) {
+					r.Get("/", paymentHandlers.GetAllByMerchantID)
+				})
+			})
+
+			r.Route("/payment", func(r chi.Router) {
+				r.With(middleware.IsCustomer).Post("/", paymentHandlers.CreatePayment)
+				r.Route("/{paymentID}", func(r chi.Router) {
+					r.Get("/", paymentHandlers.GetPayment)
+
+					r.With(middleware.IsMerchant).Post("/process", paymentHandlers.ProcessPayment)
+					r.With(middleware.IsMerchant).Post("/refund", paymentHandlers.RefundPayment)
+				})
+			})
 		})
 	})
 
